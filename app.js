bc3036843a8701a6b9921b7a641ecd5074c969d0
Ashish Kumar <akdev5520@gmail.com>
2024-01-23 20:45:44 +0530

    added JOI server-side schema validations

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const ejsMate = require('ejs-mate');
 const mongoose = require('mongoose');
 const catchAsync = require('./utils/catchAsync');
+const { campgroundSchema } = require('./schemas');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 const ExpressError = require('./utils/ExpressError');
@@ -26,6 +27,13 @@ app.use(methodOverride('_method'));
 app.use(express.urlencoded({ extended: true }));
 app.set('views', path.join(__dirname, '/views'));
 
+const validateCampground = (req, res, next) => {
+    const { error } = campgroundSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(ele => ele.message).join(',');
+        throw new ExpressError(msg, 400);
+    } else next();
+};
 
 // home route
 app.get('/', (req, res) => {
@@ -45,7 +53,7 @@ app.get('/campgrounds/new', (req, res) => {
     res.render('campgrounds/new');
 });
 
-app.post('/campgrounds', catchAsync(async (req, res, next) => {
+app.post('/campgrounds', validateCampground, catchAsync(async (req, res, next) => {
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground.id}`);
@@ -67,7 +75,7 @@ app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
     res.render('campgrounds/edit', { campground });
 }));
 
-app.put('/campgrounds/:id', catchAsync(async (req, res) => {
+app.put('/campgrounds/:id', validateCampground, catchAsync(async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
     res.redirect(`/campgrounds/${campground.id}`);
