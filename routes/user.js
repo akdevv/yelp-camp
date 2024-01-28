05a69b6eacd2a982531c7d5f07a4784541045f4d
Ashish Kumar <akdev5520@gmail.com>
2024-01-28 20:37:01 +0530

    added register route logic

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,7 @@
 // imports
 const express = require('express');
 const User = require('../models/user');
+const catchAsync = require('../utils/catchAsync');
 
 // create router instance
 const router = express.Router({ mergeParams: true });
@@ -12,9 +13,18 @@ router.get('/register', (req, res) => {
     res.render('users/register');
 });
 
-router.post('/register', async (req, res) => {
-    res.send(req.body);
-});
+router.post('/register', catchAsync(async (req, res) => {
+    try {
+        const { email, username, password } = req.body;
+        const user = new User({ email, username });
+        const registeredUser = await User.register(user, password);
+        req.flash('success', 'Welcome to Yelp Camp!');
+        res.redirect('/campgrounds');
+    } catch (err) {
+        req.flash('error', err.message);
+        res.redirect('/register');
+    }
+}));
 
 // export router
 module.exports = router;
