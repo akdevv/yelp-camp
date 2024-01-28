ef713d9b48e9a89aff7ed3cbde374f2a3483e466
Ashish Kumar <akdev5520@gmail.com>
2024-01-28 11:02:03 +0530

    configured public dir

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ app.set('view engine', 'ejs');
 app.use(methodOverride('_method'));
 app.use(express.urlencoded({ extended: true }));
 app.set('views', path.join(__dirname, '/views'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // use routes
 app.use('/campgrounds', campgroundRoutes);
