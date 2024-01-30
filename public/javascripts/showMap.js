3f70a9b6b6a6a9bee2a80ad8fcb43931b3278d00
Ashish Kumar <akdev5520@gmail.com>
2024-01-30 15:03:36 +0530

    displaying map

---
+++ public/javascripts/showMap.js
@@ -0,0 +1,8 @@
+mapboxgl.accessToken = mapboxToken;
+const map = new mapboxgl.Map({
+    container: 'map', // container ID
+    style: 'mapbox://styles/mapbox/streets-v12', // style URL
+    center: campground.geometry.coordinates, // starting position [lng, lat]
+    zoom: 10, // starting zoom
+});
+
