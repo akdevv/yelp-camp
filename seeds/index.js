5e41d1f31e7fefa23f14bed93ea94e3c178dc82e
Ashish Kumar <akdev5520@gmail.com>
2024-01-30 11:41:23 +0530

    fixing seeds/index.js

--- seeds/index.js
+++ seeds/index.js
@@ -29,8 +29,17 @@ const seedDB = async () => {
             author: '65b7ab41c7de673e96527639',
             title: `${sample(descriptors)} ${sample(places)}`,
             location: `${cities[randNum].city}, ${cities[randNum].state}`,
-            image: 'https://source.unsplash.com/collection/483251',
-            description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum quisquam architecto tenetur saepe mollitia soluta itaque, quam quod adipisci magnam iusto, quasi illo et doloremque nostrum eos perspiciatis totam ab.'
+            description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum quisquam architecto tenetur saepe mollitia soluta itaque, quam quod adipisci magnam iusto, quasi illo et doloremque nostrum eos perspiciatis totam ab.',
+            images: [
+                {
+                    url: 'https://res.cloudinary.com/da4hupi8g/image/upload/v1706594799/yelp-camp/default/e3mhbuqm5xii2kq4fzjg.jpg',
+                    filename: 'yelp-camp/default/e3mhbuqm5xii2kq4fzjg'
+                },
+                {
+                    url: 'https://res.cloudinary.com/da4hupi8g/image/upload/v1706594799/yelp-camp/default/fwgmga05uiqfft6xzn5b.jpg',
+                    filename: 'yelp-camp/default/fwgmga05uiqfft6xzn5b'
+                }
+            ]
         });
         await camp.save();
     };
