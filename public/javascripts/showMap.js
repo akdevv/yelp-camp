522bd3e66d847564099b2741c8610062e2a2c42f
Ashish Kumar <akdev5520@gmail.com>
2024-01-30 15:28:27 +0530

    centering map on campground and showing pins

--- public/javascripts/showMap.js
+++ public/javascripts/showMap.js
@@ -1,8 +1,13 @@
+// map config
 mapboxgl.accessToken = mapboxToken;
 const map = new mapboxgl.Map({
     container: 'map', // container ID
     style: 'mapbox://styles/mapbox/streets-v12', // style URL
     center: campground.geometry.coordinates, // starting position [lng, lat]
-    zoom: 10, // starting zoom
+    zoom: 10 // starting zoom
 });
 
+// adding map pin
+new mapboxgl.Marker({ color: 'red' })
+    .setLngLat(campground.geometry.coordinates)
+    .addTo(map);
